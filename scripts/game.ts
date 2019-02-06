3b9e509f63ecbe14ab4bcb76a3e751c7f561bef4
DeLucaJ <delucaj96@gmail.com>
2019-02-06 12:06:11 -0800

    convert to typescript

---
+++ scripts/game.ts
@@ -0,0 +1,32 @@
+/// <reference path='../typings/phaser.d.ts'/>
+
+class GameApp
+{
+    game: Phaser.Game;
+    config: GameConfig = {
+        type: Phaser.AUTO,
+        parent: 'game',
+        width: 800,
+        height: 600,
+        scene: [ MainScene ],
+        physics: {
+            default: 'arcade',
+            arcade: {
+                gravity: { y: 100 },
+                debug: false
+            }
+        },
+        backgroundColor: '#CCCCCC',
+        render : {
+            pixelArt: true
+        },
+    }
+
+    constructor () {
+        this.game = new Phaser.Game(this.config);
+    }
+}
+
+window.onload = () => {
+    var game = new GameApp();
+}
---
+++ scripts/main_scene.ts
@@ -0,0 +1,22 @@
+class MainScene extends Phaser.Scene  
+{
+    //world : Phaser.Physics.Arcade.World;
+
+    controls : Phaser.Cameras.Controls.FixedKeyControl;
+
+    constructor () {
+        super("MainScene");
+    }
+
+    preload () {
+        
+    }
+
+    create () {
+        
+    }
+
+    update (time, delta) {
+        
+    }
+}
