e89c55162323747435d8dcf7e31df998462772ac
DeLucaJ <delucaj96@gmail.com>
2019-02-10 23:55:54 -0800

    Adding assets

---
+++ assets/tilesets/request_sheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="request_sheet" tilewidth="70" tileheight="70" tilecount="98" columns="14">
+ <image source="request_sheet.png" width="980" height="490"/>
+</tileset>
