6678b17e0b61839b4301a15c816bc578cda8953b
DeLucaJ <delucaj96@gmail.com>
2019-02-11 18:27:11 -0800

    map updates, mountain-scene updates

--- scripts/mountain_scene.ts
+++ scripts/mountain_scene.ts
@@ -25,7 +25,36 @@ class MountainScene extends Phaser.Scene
     }
 
     create () {
+        //make tilemaps
+        const map = this.make.tilemap({key: "map"});
 
+        //add tilesets
+        const tiles = map.addTilesetImage("tiles_spritesheet", "tiles");
+        const ice = map.addTilesetImage("ice_sheet", "ice");
+
+        //create layers
+        const platformLayer = map.createStaticLayer("platforms", tiles, 0, 0);
+        const background = map.createStaticLayer("background", tiles, 0, 0);
+        const climbable = map.createStaticLayer("climable", ice, 0, 0);
+
+        //spawnpoints
+        const playerSpawn = map.findObject("spawns", obj => obj.name === "player-spawn");
+        //will probably through warnings
+        this.player = new Player(this, 700, 500);
+
+        //worldLayer.setCollisionByProperty({collides: true});
+        
+        const debugGraphics = this.add.graphics().setAlpha(0.75);
+        //this.physics.add.collider(this.player.sprite, worldLayer);
+
+        /*worldLayer.renderDebug(debugGraphics, {
+            tileColor: null, // Color of non-colliding tiles
+            collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
+            faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
+        });*/
+    
+        this.cameras.main.startFollow(this.player.sprite);
+        this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
     }
 
     update () {
